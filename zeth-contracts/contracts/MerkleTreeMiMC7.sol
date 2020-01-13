9a8fc6ac953ce0565ae2d5d59383cd9ec59e9e41
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-01-13 10:12:05 +0000

    contracts: remove all public methods except mix

--- zeth-contracts/contracts/MerkleTreeMiMC7.sol
+++ zeth-contracts/contracts/MerkleTreeMiMC7.sol
@@ -12,36 +12,8 @@ contract MerkleTreeMiMC7 is BaseMerkleTree {
     constructor(uint treeDepth) BaseMerkleTree(treeDepth) public {
     }
 
-    // Returns the current merkle tree
-    function getTree() public view returns (bytes32[] memory) {
-        uint nbNodes = 2**(depth + 1) - 1;
-        bytes32[] memory tmpTree = new bytes32[](nbNodes);
-        bytes32 left;
-        bytes32 right;
-        // Dump the leaves in the right indexes in the tree
-        for (uint i = 0; i < nbLeaves; i++) {
-            tmpTree[(nbLeaves - 1) + i] = leaves[i];
-        }
-
-        // Compute the internal nodes of the merkle tree
-        for (uint i = nbLeaves - 2; i > 0; i--) {
-            left = tmpTree[2*i+1];
-            right = tmpTree[2*(i+1)];
-
-            // Seed is hardcoded and given by "clearmatics_mt_seed"
-            tmpTree[i] = MiMC7.hash(left, right);
-        }
-
-        // Compute the merkle root
-        left = tmpTree[1];
-        right = tmpTree[2];
-        tmpTree[0] = MiMC7.hash(left, right);
-
-        return tmpTree;
-    }
-
     // Returns the root of the merkle tree
-    function getRoot() public view returns(bytes32) {
+    function getRoot() internal view returns(bytes32) {
         uint layerSize = nbLeaves / 2;
         bytes32[nbLeaves/2] memory pad;
 
